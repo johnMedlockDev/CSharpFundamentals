5a1112734a715423601408a8f3f8b054d3e3973f
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2022-01-10 17:45:25 -0600

    Loops

--- LoopsAdditionalLoopingStatements/Program.cs
+++ LoopsAdditionalLoopingStatements/Program.cs
@@ -6,7 +6,134 @@ namespace LoopsAdditionalLoopingStatements
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          // So in the last module we learned about about the foreach loop and this is really our first introduction to
+          // an abstraction interface.
+          
+          // That is just a fancy way of saying the Microsoft or what ever developer made something easier or added a layer
+          // to complexity to hide it from the user. 
+          // They do this to make development easier and maybe more readable. 
+
+          
+          // if you recall in the foreach loop I said that the foreach statement cycles through all the indexes in
+          // a collection and gives you access to the data in a temporary variable.
+          
+          #region ForLoops
+
+          // for loops can allow you do basically the same thing as a foreach loop but it assumes you want access to the index
+          // either through out the lifecycle of the loop or at least in parts.
+
+          int[] intArray =
+          {
+              1,
+              2,
+              3,
+              4,
+              5
+          };
+          
+          for (int i = 0; i < intArray.Length; i++)
+          {
+              // selected index 
+              int selectedIndex = intArray[i];
+              
+              Console.WriteLine(selectedIndex);
+          }
+          
+          // forKeyword ( dataType variableNameToRepresentAIndex = Value; variableNameToRepresentAIndex  < collection.Length; variableNameToRepresentAIndex += 1 )
+          // if you step through the loop you'll see that it's doing this basically:
+          
+          /*
+           * int selectedIndex = intArray[0];
+           * int selectedIndex = intArray[1];
+           * int selectedIndex = intArray[2];
+           * etc.
+           */
+          
+          // the foreach loop just did this without all the c++ style syntax.
+          
+          // for loops are flexible in the fact that you can go forward and backwards:
+          // the <  intArray.Length thing gets the number of values in the array. 
+          
+          for (int i = intArray.Length - 1; i >= 0; i--)
+          {
+              // selected index 
+              int selectedIndex = intArray[i];
+              
+              Console.WriteLine(selectedIndex);
+          }
+          
+          // In this instance it starts at the end of the array and goes to the first value.
+          
+          // Just to kind of way break it done a little more this is kinda how it works:
+          // for (
+          //
+          //   int i = 0;
+          //   if(i < intArray.Length)
+          //   {
+          //     // selected index 
+          //     int selectedIndex = intArray[i];
+          //     
+          //     Console.WriteLine(selectedIndex);
+          //   }
+          //     i++;
+          // )
+
+          #endregion ForLoops
+
+          #region WhileLoops
+          
+          // while loops can do the same thing as a for loop or foreach loop but it's really best to use when you don't
+          // know when the loop will stop or maybe if the loop will never stop.
+
+          // this is how you would write a loop that would never end.
+          // it will continue to run until the value in the () turns false. 
+          // because nothing effects it to change the state if ran it will run for ever.
+          // while (true)
+          // {
+          //     
+          // }
+
+          int value = 0;
+
+          while (value != 4)
+          {
+              // a way to generate sudo random values 
+              Random random = new();
+
+              value = random.Next(0, intArray.Length);
+
+              Console.WriteLine(value);
+          }
+          
+          // in this instance the value is selected sudo randomly until the value == 4.
+          // this would be a good instance for a while loop. 
+          // a for loop couldn't be used because the value may never == 4.
+          
+          while (false)
+          {
+              
+          }
+          
+          // on the contrary to the first while example this while loop will never run because it never evaluates () to true.
+          
+          #endregion WhileLoops
+          
+          #region DoWhileLoops
+          
+          // I'm just going to briefly go over Do While loops. They work the same way as while loops but the code in the
+          // loop always runs once regardless of the condition of the while loop (). But after it runs once it takes into account
+          // the loop condition.
+
+          do
+          {
+              Console.WriteLine("Do While Ran.");
+          }
+          while (false);
+          
+          // So you use a do while if you want to write a loop that always executes at least once.
+
+          #endregion DoWhileLoops
+
         }
     }
 }
--- LoopsTheForEachStatement/Program.cs
+++ LoopsTheForEachStatement/Program.cs
@@ -6,7 +6,109 @@ namespace LoopsTheForEachStatement
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Ok so up until this point we've gone over variables, data types, and conditional statements.
+            // And we can do a lot of powerful stuff compared to where we first started.
+            // But really our operations are really still manual.
+            // By that I mean we're still putting in a action or series of actions to get the the same number of results.
+            // With loops you can program a single operation and then just "Loop" or cycle through the rest of the actions.
+            // Like lets say we have an array of ints and we want to add the numbers 1 - 10 to the array.
+            
+            // We would have to do it something like this:
+
+            int[] intArray = new[]
+            {
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10
+            };
+            
+            // or this:
+
+            int[] intArray2 = new int[10];
+
+            intArray2[0] = 1;
+            intArray2[1] = 2;
+            intArray2[2] = 3;
+            intArray2[3] = 4;
+            intArray2[4] = 5;
+            intArray2[5] = 6;
+            intArray2[6] = 7;
+            intArray2[7] = 8;
+            intArray2[8] = 9;
+            intArray2[9] = 10;
+            
+            // Now imagine if we had to do this 100,000 times. 
+            // That would be a lot of typing for one two quite error prone.
+
+            // With loops we can do something like this with just a few lines of code.
+
+            int[] intArray3 = new int[10];
+            
+            int counter = 0;
+            
+            foreach (int space in intArray3)
+            {
+                int valueForSpace = counter + 1;
+                intArray3[counter] = valueForSpace;
+                counter++;
+            }
+            
+            // maybe for ten values you can't appreciate the difference but for 100,000 you could
+            int[] intArray4 = new int[100000];
+            counter = 0;
+            
+            foreach (int space in intArray4)
+            {
+                int valueForSpace = counter + 1;
+                intArray3[counter] = valueForSpace;
+                counter++;
+            }
+            
+            /*
+             *  foreach ( datatype variableName inKeyword collection )  
+             *  {
+             *      * Code *
+             *  }
+             */
+            
+            // What the foreach statement does is cycle through all of the indexes in a collection until
+            // it reaches the end.
+            // This is useful when you have to do a similar operation for a series.
+            // This is one of the more common loops you'll use because most of the time you don't actually need the index.
+            // You just need to modify the state of what is contained in the collection.
+            // You can use the foreach look any more than just ints but this was just an example.
+            
+            // Indexes are the marker for a specific place in an array. 
+            // the variableName can be called whatever you want but just like with normal variables it is a good idea to 
+            // make the value you place there make since in the context of the collection.
+            // Say you had a array of names a foreach statement like this might make sense.
+            // foreach(string name in names)
+            // {
+            //    * Code * 
+            // }
+            
+            // for the code above you would have made argument for using what is referred to as a discard variable
+            // because we didn't actually end up using the variable itself. _ <= that is regarded as a discard variable. 
+            // foreach(int _ in intArray4)
+
+            string[] names = {"John", "Justin", "Anne"};
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            
+            // Set some break points and step through the loops to check your understanding.
+
+
+
         }
     }
 }
